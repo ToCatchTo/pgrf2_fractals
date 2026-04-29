dd9b16f613c97d3e9df12c95d1cc08a47ae101f0
Tomas <tomas@terc.cz>
2026-04-29 12:05:39 +0200

    edit

--- src/main/java/utils/TextureLoader.java
+++ src/main/java/utils/TextureLoader.java
@@ -1,8 +1,13 @@
 package utils;
 
 import org.lwjgl.BufferUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.stb.STBImage.*;
@@ -13,33 +18,62 @@ public class TextureLoader {
         IntBuffer height = BufferUtils.createIntBuffer(1);
         IntBuffer channels = BufferUtils.createIntBuffer(1);
 
-        String fixedFilePath = "src/main/resources/textures/" + filepath;
+        String resourcePath = "/textures/" + filepath;
 
-        // STB image načítá obrázky shora dolů, OpenGL zdola nahoru, takže obrázek převrátíme
         stbi_set_flip_vertically_on_load(true);
 
-        // Načtení obrázku do ByteBufferu
-        ByteBuffer image = stbi_load(fixedFilePath, width, height, channels, 4);
+        ByteBuffer imageBuffer;
+        try {
+            imageBuffer = ioResourceToByteBuffer(resourcePath);
+        } catch (IOException e) {
+            throw new RuntimeException("Nepodařilo se najít texturu: " + resourcePath, e);
+        }
+
+        ByteBuffer image = stbi_load_from_memory(imageBuffer, width, height, channels, 4);
         if (image == null) {
             throw new RuntimeException("Failed to load a texture file! " + System.lineSeparator() + stbi_failure_reason());
         }
 
-        // Vygenerování ID textury v OpenGL
         int textureId = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, textureId);
 
-        // Nastavení parametrů textury (jak se má chovat při zvětšení/zmenšení)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-        // Odeslání dat do grafické karty
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 
-        // Uvolnění paměti z RAM (už to je ve VRAM grafiky)
         stbi_image_free(image);
 
         return textureId;
+    }
+
+    private static ByteBuffer ioResourceToByteBuffer(String resource) throws IOException {
+        ByteBuffer buffer;
+
+        InputStream source = TextureLoader.class.getResourceAsStream(resource);
+
+        if (source == null) {
+            throw new IOException("Resource not found: " + resource);
+        }
+
+        try (ReadableByteChannel rbc = Channels.newChannel(source)) {
+            buffer = BufferUtils.createByteBuffer(1024 * 1024);
+
+            while (true) {
+                int bytes = rbc.read(buffer);
+                if (bytes == -1) break;
+
+                if (buffer.remaining() == 0) {
+                    ByteBuffer newBuffer = BufferUtils.createByteBuffer(buffer.capacity() * 2);
+                    buffer.flip();
+                    newBuffer.put(buffer);
+                    buffer = newBuffer;
+                }
+            }
+        }
+        buffer.flip();
+        return buffer;
     }
 }
